bad5ec173a13070e21d3552f5520a7fcae8241f7
TVke <thomas.verhelst@student.kdg.be>
2018-01-21 01:56:53 +0100

    initial commit

---
+++ resources/lang/nl/general.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Home Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the construction of the multiple
+    | pages in this project.
+    |
+    */
+
+    'title'   => 'het eigenaardige doolhof',
+    'description-home'   => '',
+    'description-general'   => '',
+
+];
---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title'){{ __('general.title') }}</title>
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    @if(Route::currentRouteName() === "home")
+        <meta name="description" content="{{ __('general.description-home') }}">
+    @else
+        <meta name="description" content="{{ __('general.description-general') }}">
+    @endif
+    @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+        <link rel="alternate" hreflang="{{ $localeCode }}" href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
+    @endforeach
+</head>
+<body>
+@yield('content')
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
