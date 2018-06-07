d4e3c61a5d595b4b5e409ca89569e38b0b05f7b5
TVke <thomas_verhelst@telenet.be>
2018-06-07 09:40:39 +0200

    Add security headers

---
+++ app/Http/Middleware/SecurityHeaders.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class SecurityHeaders
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        $response->header('strict-transport-security', 'max-age=31536000');
+        $response->header('x-frame-options', 'SAMEORIGIN');
+        $response->header('X-XSS-Protection', '1; mode=block');
+        $response->header('Referrer-Policy', 'strict-origin');
+        $response->header('Server', '');
+        $response->header('Expect-CT', 'enforce;max-age=3600');
+
+        return $response;
+    }
+}
